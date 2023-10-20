63bcf44591ef3251024a10b3a1f24805141c8520
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-10-20 18:40:25 +0300

    upd

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,57 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected tenants: Person[];
+  protected door: boolean;
+
+  constructor(protected key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person) {
+    if (this.door) {
+      return this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open");
+    } else {
+      console.log("Invalid key.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +60,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
